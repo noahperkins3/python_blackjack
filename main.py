fbfd5886daa5cf96d5bfd85c3012dd7c14d80030
noahperkins3 <noahperkins3@gmail.com>
2023-09-03 20:03:55 -0700

    Added game logic

--- main.py
+++ main.py
@@ -2,21 +2,92 @@ import card
 import deck
 import player
 
-starting_amount = 500
+cash_amount = 500
+thresh = 21
 
 running = True
+dealer = player.Dealer(deck.Deck())
+player = player.Player(deck.Deck())
+
+def checkWin(player, dealer):
+    if player.getTotal() > dealer.getTotal() and player.getTotal() < 21:
+        return True
+    else:
+        return False
 
 while running:
-    print("Welcome to Blackjack! Your starting amount is $" + str(starting_amount))
+    new_deck = deck.Deck()
+    new_deck.shuffle()
+    dealer.resetDeck(new_deck)
+    player.resetDeck(new_deck)
+    game_over = False
+    print("Welcome to Blackjack!\n\nYour starting amount is $" + str(cash_amount))
+    play = input("Would you like to play a hand? ")
+    if play[0] == 'y' or play[0] == 'Y':
+        bet_amount = int(input("Place your bet: "))
+        while bet_amount < 1:
+            print("The minimum bet is $1. ")
+            bet_amount = int(input("Place your bet: "))
+        while bet_amount > cash_amount:
+            print("Insufficient funds! ")
+            bet_amount = int(input("Place your bet: "))
+        # first deal
+        for i in range(2):
+            player.hit(new_deck.pullTopCard())
+            dealer.hit(new_deck.pullTopCard())
+        print("You are dealt: " + player.__str__())
+        print("The dealer is dealt: " + dealer.__str__())
+        if player.getTotal() == thresh:
+            if not dealer.getTotal() == thresh:
+                print("Your starting hand is a blackjack, you win!")
+                cash_amount += bet_amount * 1.5
+            else:
+                print("You and the dealer were dealt blackjack, tie.")
+        else:
+            while not game_over:
+                hit_or_stay = input("Would you like to hit or stay? ")
+                while not hit_or_stay == 'hit' and not hit_or_stay == 'stay':
+                    print("That is not a valid option. ")
+                    hit_or_stay = input("Would you like to hit or stay? ")
+                if hit_or_stay == 'hit':
+                    player.hit(new_deck.pullTopCard())
+                    print("You are dealt: " + player.getHand()[-1].__str__())
+                    print("You now have: " + player.__str__())
+                    if player.getTotal() > thresh:
+                        print("Your hand value is over 21 and you lose " + str(bet_amount) + " :(")
+                        cash_amount -= bet_amount
+                        if cash_amount <= 0:
+                            print("\nYou've ran out of money. Please restart the program to try again. See you!")
+                            running = False
+                        game_over = True
+                elif hit_or_stay == 'stay':
+                    dealer.setHide(False)
+                    print("The dealer has: " + dealer.__str__())
+                    while dealer.getTotal() <= 16:
+                        dealer.hit(new_deck.pullTopCard())
+                        print("The dealer hits and is dealt: " + dealer.getHand()[-1].__str__())
+                    print("The dealer stays.")
+                    if checkWin(player, dealer):
+                        print("You win $" + bet_amount + "!")
+                    else:
+                        print("The dealer wins and you lose " + str(bet_amount) + " :(")
+                        if cash_amount <= 0:
+                            print("\nYou've ran out of money. Please restart the program to try again. See you!")
+                            running = False
+                    game_over = True
+    else:
+        print("You left with $" + str(cash_amount) + " in hand. See you!")
+        running = False
 
-    
+
+ 
 
 # player = player.Player()
 # deck = deck.Deck()
 
 # deck.shuffle()
-# player.addCard(deck.pullTopCard())
-# player.addCard(deck.pullTopCard())
+# player.hit(deck.pullTopCard())
+# player.hit(deck.pullTopCard())
 
 
 # print(player.__str__())
--- player.py
+++ player.py
@@ -2,22 +2,69 @@ import card
 
 class Player:
 
-    def __init__(self):
+    def __init__(self, deck):
+        self.deck = deck
         self.starting_amount = 500
         self.total = 0
         self.hand = []
 
+    def firstDeal(self):
+        for i in range(2):
+            self.hand.append(self.deck.pullTopCard())
 
-    def addCard(self, card):
+    def hit(self, card):
         self.hand.append(card)
         self.total += card.getValue(self)
+
+    def getHand(self):
+        return self.hand
 
     def getTotal(self):
         return self.total
 
+    def resetDeck(self, deck):
+        self.deck = deck
 
     def __str__(self):
         res = ''
         for card in self.hand:
             res += card.__str__() + ', '
         return res[0:(len(res)-2)]
+    
+class Dealer:
+    def __init__(self, deck):
+        self.deck = deck
+        self.starting_amount = 500
+        self.total = 0
+        self.hand = []
+        self.hide = True
+
+    def firstDeal(self):
+        for i in range(2):
+            self.hand.append(self.deck.pullTopCard())
+
+    def hit(self, card):
+        self.hand.append(card)
+        self.total += card.getValue(self)
+
+    def getHand(self):
+        return self.hand
+
+    def getTotal(self):
+        return self.total
+    
+    def setHide(self, hide):
+        self.hide = hide
+
+    def resetDeck(self, deck):
+        self.deck = deck
+
+    def __str__(self):
+        res = ''
+        if self.hide:
+            res += self.hand[0].__str__() + ", Unknown"
+        else:
+            for card in self.hand:
+                res += card.__str__() + ', '
+            res = res[0:(len(res)-2)]
+        return res
